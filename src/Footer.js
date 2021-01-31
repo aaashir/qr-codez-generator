9638ff2dc6df0ec52f9898551faa4397c51e4095
Aashir Shaikh <hi@aashir.me>
2021-01-31 23:49:57 +0500

    pilot

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Footer.css";
+import { FaHeart } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__content">
         <p>
-          made with ❤ by <b>aaashir</b>
+          made with <FaHeart /> by{" "}
+          <b class="footer__link">
+            <a href="https://github.com/aaashir">aaashir</a>
+          </b>
         </p>
       </div>
     </div>
